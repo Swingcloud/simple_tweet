be9282269c306107e60d536adf9c243005409d71
Sam Lin <greenlin111@gmail.com>
2017-09-26 10:20:04 +0800

    add edit on TweetsController

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,6 +19,19 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to :root
+    else
+      render :edit
+    end
+  end
+
   private
 
   def tweet_params
